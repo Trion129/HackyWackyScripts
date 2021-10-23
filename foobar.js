2311f4787e5a585518e40573e6485c755054ea94
Parminder Singh <parmsingh129@gmail.com>
2021-10-23 12:33:12 +0530

    Create foobar.js

---
+++ foobar.js
@@ -0,0 +1,6 @@
+// Generate Google foobar invite, go to any website, open console and paste and run the following
+// NOTE: Disable popup blocker.
+// On one of the tabs you'll see the foobar invite generated.
+for(var i = 0; i < 10; i++){
+    window.open("https://google.com/search?q=arraylist+java");
+}
